8756e64872c05a5d2d10a2dcf6ee1708ff637c8f
Prabhat Pandey <160058150+Prabhat9572@users.noreply.github.com>
2024-12-07 23:21:01 +0530

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,29 +9,34 @@ const Header = () => {
     setMenuOpen(!menuOpen);
   };
 
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <header className="studentsupport-header">
       <div className="header-container">
         <div className="logo">
           <span>Students Support</span>
         </div>
+
+        {/* Toggle button for mobile view */}
         <div className="toggle-button" onClick={toggleMenu}>
           <span className="bar"></span>
           <span className="bar"></span>
           <span className="bar"></span>
         </div>
+
+        {/* Navbar */}
         <nav className={`nav-menu ${menuOpen ? 'open' : ''}`}>
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/schedule">Schedule</Link></li>
-            <li><Link to="/speakers">Speakers</Link></li>
-            <li><Link to="#contact">Contact</Link></li>
+            <li><Link to="/" onClick={closeMenu}>Home</Link></li>
+            <li><Link to="/about" onClick={closeMenu}>About</Link></li>
+            <li><Link to="/schedule" onClick={closeMenu}>Schedule</Link></li>
+            <li><Link to="/speakers" onClick={closeMenu}>Speakers</Link></li>
+            <li><Link to="#contact" onClick={closeMenu}>Contact</Link></li>
           </ul>
         </nav>
-        <div className="cta-button">
-          <a href="#register" className="btn-register">Sign In</a>
-        </div>
       </div>
     </header>
   );
